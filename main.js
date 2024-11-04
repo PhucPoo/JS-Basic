f895a81938563dcceddd6e8eba1d96114ed1e6ba
PhucPoo <hoangphuc1161998@gmail.com>
2024-11-04 21:13:35 +0700

    Buoi 1

---
+++ main.js
@@ -0,0 +1,53 @@
+// // Khai báo biến
+// var fullName = 'Chó Thảo!!!!';
+// var age = 20
+
+// // Gọi hàm thông báo
+// alert (fullName);
+// alert (age);
+
+// // Hàm console
+// console.log(age);
+
+// // Hàm confirm giúp bạn xác nhận 
+// confirm('Xác nhận giúp bạn đủ tuổi');
+
+// // Hàm prompt giúp điền vào hộp thoại
+// prompt('Xác nhận giúp bạn đủ tuổi');
+
+// // Hàm setTimeout 
+// setTimeout (function() {
+//     alert('Thông báo')
+// }, 1000)
+
+// // Toán tử trong JS
+var a = 1;
+var b = 2;
+//  if(a < b) {
+//     console.log('Yes')
+//  }
+// //  các phép tính trong js
+//  var c = a + b;
+//  cộng chuõi
+var firstName = 'Hoàng';
+var lastName = 'Phúc';
+console.log(firstName + ' '+ lastName);
+
+if(a < b) {
+    console.log('Yes');
+}
+else {
+    console.log('No')
+}
+// Boolean
+var isSuccess = true;
+console.log(isSuccess)
+/*
+Trong Js cứ gặp 6 trường hợp sau là False
+0
+false
+NaN
+null
+''-""
+undefined
+*/
